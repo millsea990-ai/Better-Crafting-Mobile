c0db5f1616ddc24f825db0ed23d9ae0acf7d1367
millsea990-ai <millsea990@gmail.com>
2026-02-20 07:03:39 +0300

    Update BetterCraftingManager.cs

--- BetterCraftingManager.cs
+++ BetterCraftingManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using HarmonyLib;
 using Microsoft.Xna.Framework;
 using StardewModdingAPI;
+using StardewModdingAPI.Events;
 using StardewValley;
 using StardewValley.Menus;
 using StardewValley.Objects;
